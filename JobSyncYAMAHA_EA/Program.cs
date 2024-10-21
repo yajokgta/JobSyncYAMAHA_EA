30fb56f77643d37583036bc0aea05f7778b88dc6
Kitisak <kitisak@techconsbiz.com>
2024-10-21 10:25:26 +0700

    .

--- JobSyncYAMAHA_EA/Program.cs
+++ JobSyncYAMAHA_EA/Program.cs
@@ -15,65 +15,65 @@ namespace JobSyncYAMAHA_EA
             //var tran = _context.Transaction.Connection.BeginTransaction();
             try
             {
-                var viewAll = _context.V_EMPLOYEE_TYMs.ToList();
+                //var viewAll = _context.V_EMPLOYEE_TYMs.ToList();
 
 
-                foreach (var viewEmp in viewAll)
-                {
-                    var positionQuery = _context.MSTPositions.Where(x => x.NameEn == viewEmp.NAMPOS || x.NameTh == viewEmp.NAMPOS);
-                    if (!positionQuery.Any(x => x.NameEn == viewEmp.NAMPOS || x.NameTh == viewEmp.NAMPOS))
-                    {
-                        var position = new MSTPosition();
-                        position.CreatedDate = DateTime.Now;
-                        position.ModifiedDate = DateTime.Now;
-                        position.IsActive = true;
-                        position.NameEn = viewEmp.NAMPOS.Replace(Environment.NewLine, "").Trim();
-                        position.NameTh = viewEmp.NAMPOS.Replace(Environment.NewLine, "").Trim();
-                        position.CreatedBy = "SYSTEM";
-                        position.ModifiedBy = "SYSTEM";
-                        position.CompanyCode = "TYM";
-                        _context.MSTPositions.InsertOnSubmit(position);
-                    }
+                //foreach (var viewEmp in viewAll)
+                //{
+                //    var positionQuery = _context.MSTPositions.Where(x => x.NameEn == viewEmp.NAMPOS || x.NameTh == viewEmp.NAMPOS);
+                //    if (!positionQuery.Any(x => x.NameEn == viewEmp.NAMPOS || x.NameTh == viewEmp.NAMPOS))
+                //    {
+                //        var position = new MSTPosition();
+                //        position.CreatedDate = DateTime.Now;
+                //        position.ModifiedDate = DateTime.Now;
+                //        position.IsActive = true;
+                //        position.NameEn = viewEmp.NAMPOS.Replace(Environment.NewLine, "").Trim();
+                //        position.NameTh = viewEmp.NAMPOS.Replace(Environment.NewLine, "").Trim();
+                //        position.CreatedBy = "SYSTEM";
+                //        position.ModifiedBy = "SYSTEM";
+                //        position.CompanyCode = "TYM";
+                //        _context.MSTPositions.InsertOnSubmit(position);
+                //    }
 
-                    var deptQuery = _context.MSTDepartments.Where(x => x.NameEn == viewEmp.NAMCENTENG || x.NameTh == viewEmp.NAMCENTHA);
-                    if (!deptQuery.Any(x => x.NameEn == viewEmp.NAMCENTENG || x.NameTh == viewEmp.NAMCENTHA))
-                    {
-                        var dept = new MSTDepartment();
-                        dept.CreatedDate = DateTime.Now;
-                        dept.ModifiedDate = DateTime.Now;
-                        dept.IsActive = true;
-                        dept.NameEn = viewEmp.NAMCENTENG;
-                        dept.NameTh = viewEmp.NAMCENTHA;
-                        dept.CreatedBy = "SYSTEM";
-                        dept.ModifiedBy = "SYSTEM";
-                        dept.CompanyCode = "TYM";
-                        dept.DepartmentCode = !string.IsNullOrEmpty(viewEmp.CODCOMP) ? viewEmp.CODCOMP : null;
-                        _context.MSTDepartments.InsertOnSubmit(dept);
-                    }
+                //    var deptQuery = _context.MSTDepartments.Where(x => x.NameEn == viewEmp.NAMCENTENG || x.NameTh == viewEmp.NAMCENTHA);
+                //    if (!deptQuery.Any(x => x.NameEn == viewEmp.NAMCENTENG || x.NameTh == viewEmp.NAMCENTHA))
+                //    {
+                //        var dept = new MSTDepartment();
+                //        dept.CreatedDate = DateTime.Now;
+                //        dept.ModifiedDate = DateTime.Now;
+                //        dept.IsActive = true;
+                //        dept.NameEn = viewEmp.NAMCENTENG;
+                //        dept.NameTh = viewEmp.NAMCENTHA;
+                //        dept.CreatedBy = "SYSTEM";
+                //        dept.ModifiedBy = "SYSTEM";
+                //        dept.CompanyCode = "TYM";
+                //        dept.DepartmentCode = !string.IsNullOrEmpty(viewEmp.CODCOMP) ? viewEmp.CODCOMP : null;
+                //        _context.MSTDepartments.InsertOnSubmit(dept);
+                //    }
 
-                    var divQuery = _context.MSTDivisions.Where(x => x.NameEn == viewEmp.department_e || x.NameTh == viewEmp.department_t);
-                    if (!deptQuery.Any(x => x.NameEn == viewEmp.department_e || x.NameTh == viewEmp.department_t))
-                    {
-                        var div = new MSTDivision();
-                        div.CreatedDate = DateTime.Now;
-                        div.ModifiedDate = DateTime.Now;
-                        div.IsActive = true;
-                        div.NameEn = viewEmp.department_e;
-                        div.NameTh = viewEmp.department_t;
-                        div.CreatedBy = "SYSTEM";
-                        div.ModifiedBy = "SYSTEM";
-                        _context.MSTDivisions.InsertOnSubmit(div);
-                    }
+                //    var divQuery = _context.MSTDivisions.Where(x => x.NameEn == viewEmp.department_e || x.NameTh == viewEmp.department_t);
+                //    if (!deptQuery.Any(x => x.NameEn == viewEmp.department_e || x.NameTh == viewEmp.department_t))
+                //    {
+                //        var div = new MSTDivision();
+                //        div.CreatedDate = DateTime.Now;
+                //        div.ModifiedDate = DateTime.Now;
+                //        div.IsActive = true;
+                //        div.NameEn = viewEmp.department_e;
+                //        div.NameTh = viewEmp.department_t;
+                //        div.CreatedBy = "SYSTEM";
+                //        div.ModifiedBy = "SYSTEM";
+                //        _context.MSTDivisions.InsertOnSubmit(div);
+                //    }
 
-                    _context.SubmitChanges();
-                }
+                //    _context.SubmitChanges();
+                //}
 
                 var updates = _context.V_EMPLOYEE_TYMs.Where(x => _context.MSTEmployees.Select(s => s.EmployeeCode).Contains(x.CODEMPID)).ToList();
 
-                var inserts = _context.V_EMPLOYEE_TYMs.Where(x => !_context.MSTEmployees.Select(s => s.EmployeeCode).Contains(x.CODEMPID)).ToList();
+                //var inserts = _context.V_EMPLOYEE_TYMs.Where(x => !_context.MSTEmployees.Select(s => s.EmployeeCode).Contains(x.CODEMPID)).ToList();
 
                 Console.WriteLine($"EMP UPDATE COUNT : {updates.Count()}");
-                Console.WriteLine($"EMP INSERT COUNT : {inserts.Count()}");
+                //Console.WriteLine($"EMP INSERT COUNT : {inserts.Count()}");
 
                 var empUpdates = _context.MSTEmployees.Where(x => updates.Select(s => s.CODEMPID).Contains(x.EmployeeCode)).ToList();
 
@@ -99,36 +99,36 @@ namespace JobSyncYAMAHA_EA
                     _context.SubmitChanges();
                 }
 
-                foreach (var mapper in inserts)
-                {
-                    var insertModel = new MSTEmployee();
+                //foreach (var mapper in inserts)
+                //{
+                //    var insertModel = new MSTEmployee();
 
-                    insertModel.Username = !string.IsNullOrEmpty(mapper.CODNATNL) && mapper.CODNATNL == "01" ? "CN" + mapper.CODEMPID : mapper.CODEMPID;
-                    insertModel.EmployeeCode = mapper.CODEMPID;
-                    insertModel.NameTh = mapper.NAMEMPT;
-                    insertModel.NameEn = mapper.NAMEMPE;
-                    insertModel.Email = mapper.EMAIL;
-                    insertModel.PositionId = _context.MSTPositions.FirstOrDefault(x => x.NameEn == mapper.NAMPOS || x.NameTh == mapper.NAMPOS)?.PositionId;
-                    insertModel.DepartmentId = _context.MSTDepartments.FirstOrDefault(x => x.NameEn == mapper.NAMCENTENG || x.NameTh == mapper.NAMCENTHA)?.DepartmentId;
-                    insertModel.DivisionId = _context.MSTDivisions.FirstOrDefault(x => x.NameEn == mapper.department_e || x.NameTh == mapper.department_t)?.DivisionId;
+                //    insertModel.Username = !string.IsNullOrEmpty(mapper.CODNATNL) && mapper.CODNATNL == "01" ? "CN" + mapper.CODEMPID : mapper.CODEMPID;
+                //    insertModel.EmployeeCode = mapper.CODEMPID;
+                //    insertModel.NameTh = mapper.NAMEMPT;
+                //    insertModel.NameEn = mapper.NAMEMPE;
+                //    insertModel.Email = mapper.EMAIL;
+                //    insertModel.PositionId = _context.MSTPositions.FirstOrDefault(x => x.NameEn == mapper.NAMPOS || x.NameTh == mapper.NAMPOS)?.PositionId;
+                //    insertModel.DepartmentId = _context.MSTDepartments.FirstOrDefault(x => x.NameEn == mapper.NAMCENTENG || x.NameTh == mapper.NAMCENTHA)?.DepartmentId;
+                //    insertModel.DivisionId = _context.MSTDivisions.FirstOrDefault(x => x.NameEn == mapper.department_e || x.NameTh == mapper.department_t)?.DivisionId;
 
-                    //insertModel.ReportToEmpCode = _context.MSTEmployees.FirstOrDefault(x =>
-                    //x.EmployeeCode == (!string.IsNullOrEmpty(mapper.CODNATNL) && mapper.CODNATNL == "01" ? "CN" + mapper.codeHead : mapper.codeHead))
-                    //    ?.EmployeeId.ToString();
-                    insertModel.ReportToEmpCode = _context.MSTEmployees.Where(x => x.EmployeeCode == mapper.codeHead && x.IsActive == true).FirstOrDefault()?.EmployeeId.ToString() ?? null;
+                //    //insertModel.ReportToEmpCode = _context.MSTEmployees.FirstOrDefault(x =>
+                //    //x.EmployeeCode == (!string.IsNullOrEmpty(mapper.CODNATNL) && mapper.CODNATNL == "01" ? "CN" + mapper.codeHead : mapper.codeHead))
+                //    //    ?.EmployeeId.ToString();
+                //    insertModel.ReportToEmpCode = _context.MSTEmployees.Where(x => x.EmployeeCode == mapper.codeHead && x.IsActive == true).FirstOrDefault()?.EmployeeId.ToString() ?? null;
 
-                    insertModel.IsActive = true;
-                    insertModel.Lang = "EN";
-                    insertModel.AccountId = 1;
-                    insertModel.ADTitle = string.Empty;
+                //    insertModel.IsActive = true;
+                //    insertModel.Lang = "EN";
+                //    insertModel.AccountId = 1;
+                //    insertModel.ADTitle = string.Empty;
 
-                    insertModel.ModifiedBy = "SYSTEM";
-                    insertModel.ModifiedDate = DateTime.Now;
-                    insertModel.CreatedBy = "SYSTEM";
-                    insertModel.CreatedDate = DateTime.Now;
-                    _context.MSTEmployees.InsertOnSubmit(insertModel);
-                    _context.SubmitChanges();
-                }
+                //    insertModel.ModifiedBy = "SYSTEM";
+                //    insertModel.ModifiedDate = DateTime.Now;
+                //    insertModel.CreatedBy = "SYSTEM";
+                //    insertModel.CreatedDate = DateTime.Now;
+                //    _context.MSTEmployees.InsertOnSubmit(insertModel);
+                //    _context.SubmitChanges();
+                //}
                 //tran.Commit();
             }
 
